b2c3f0f94c610dbb6dbc8d24312e9d86c5407866
gorhgorh <gorhgorh@lesnodebots.eu>
2017-01-03 01:47:10 +0100

    update calibration script

--- calibrate.js
+++ calibrate.js
@@ -1,24 +1,43 @@
 const five = require('johnny-five')
 const board = new five.Board()
 
-board.on('open', function () {
-  let attempts = 0
-  console.log('serial port is open, testing firmata ...')
-  setInterval(function () {
-    console.log('waiting for firmata ready event ...')
-    if (attempts === 4) {
-      console.log('can\'t communicate with firmata, please reflash the board')
-      process.exit()
-    }
-    ++attempts
-  }, 2000)
+function testLaser (lzr) {
+  console.log('Test blink start!')
+  lzr.blink(500)
+  setTimeout(function () {
+    console.log('Blink off')
+    lzr.stop()
+    lzr.off()
+  }, 10000)
+}
+
+board.on('ready', function () {
+  // Create a standard `led` component instance
+  var led = new five.Led(13)
+  var servoX = new five.Servo(10)
+  var servoY = new five.Servo(11)
+
+  console.log('centering servo, you can now point the lazer in front of you to calibrate it to 90° 90°')
+  console.log('servo and laser injected in the repl')
+  console.log('servoX, servoY, laser')
+  console.log('ex : servoX.to(30)')
+
+  servoX.to(90)
+  servoY.to(90)
+
+  testLaser(led)
+
+  this.repl.inject({
+    // Allow limited on/off control access to the
+    // Led instance from the REPL.
+    on: function () {
+      led.on()
+    },
+    off: function () {
+      led.off()
+    },
+    laser: led,
+    servoX,
+    servoY
+  })
 })
-// board.on('ready', function () {
-//   // Create a standard `led` component instance
-//   var led = new five.Led(13)
-//   console.log('your board is ready to use, exiting')
-//   process.exit()
-//   // "blink" the led in 500ms
-//   // on-off phase periods
-//   led.blink(500)
-// })
