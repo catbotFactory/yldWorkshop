49d22d9e9d48b659cd6acb0e5d88c4f0d991ae2e
gorhgorh <gorhgorh@lesnodebots.eu>
2017-01-07 21:35:40 +0100

    test laser done

--- calibrate.js
+++ calibrate.js
@@ -1,43 +1,134 @@
-const five = require('johnny-five')
-const board = new five.Board()
+const os = require('os')
+const inc = require('inquirer')
+const async = require('async')
+const debug = require('debug')('catbot:calibrate')
 
-function testLaser (lzr) {
-  console.log('Test blink start!')
-  lzr.blink(500)
-  setTimeout(function () {
-    console.log('Blink off')
-    lzr.stop()
-    lzr.off()
-  }, 10000)
+const chalk = require('chalk')
+const green = chalk.green
+const red = chalk.red
+const blue = chalk.cyan
+
+
+
+const five = require('johnny-five')
+
+const laserPrompt = {
+  type: 'confirm',
+  name: 'didLaserBlinked',
+  message: 'did the laser blinked?'
 }
 
-board.on('ready', function () {
-  // Create a standard `led` component instance
+function testLaser (lzr, cb) {
+  console.log(blue('Testing laser'))
+  console.log(blue('Test blink start!'))
+  lzr.blink(200)
+  setTimeout(function () {
+    console.log(blue('Blink off'))
+    lzr.stop()
+    lzr.off()
+    inc.prompt([laserPrompt]).then(function (answers) {
+      debug(answers)
+      if (answers.didLaserBlinked === true) {
+        if (cb) cb(null, 'servoCenter')
+      } else {
+        testLaser(lzr, cb)
+      }
+    })
+  }, 1000)
+}
+
+const servoPrompt = {
+  type: 'confirm',
+  name: 'didServoSweep',
+  message: 'did the servo swept ?'
+}
+
+
+function testServos (servoX, servoY, cb) {
+  console.log(blue('Centering servo, you can now point the lazer in front of you to calibrate it to 90° 90°'))
+  servoX.to(90)
+  servoY.to(90)
+
+  setTimeout(function () {
+    console.log(blue('end centering sweeping servo X'))
+    servoX.sweep()
+    setTimeout(function () {
+      servoX.stop()
+      servoX.to(90)
+      inc.prompt([servoPrompt]).then(function (answers) {
+        debug(answers)
+      })
+    }, 2000)
+  }, 1000)
+}
+
+// create the catboard instance
+function catBoard () {
   var led = new five.Led(13)
   var servoX = new five.Servo(10)
   var servoY = new five.Servo(11)
 
-  console.log('centering servo, you can now point the lazer in front of you to calibrate it to 90° 90°')
-  console.log('servo and laser injected in the repl')
-  console.log('servoX, servoY, laser')
-  console.log('ex : servoX.to(30)')
-
-  servoX.to(90)
-  servoY.to(90)
-
-  testLaser(led)
-
-  this.repl.inject({
-    // Allow limited on/off control access to the
-    // Led instance from the REPL.
-    on: function () {
-      led.on()
+  async.series([
+    function (cb) {
+      // do some stuff ...
+      testLaser(led, cb)
     },
-    off: function () {
-      led.off()
-    },
-    laser: led,
-    servoX,
-    servoY
+    function (cb) {
+      // do some more stuff ...
+      testServos(servoX, servoY, cb)
+    }
+  ],
+    // optional callback
+    function (err, results) {
+      if (err) console.log(err)
+      debug(results)
+    })
+
+
+}
+
+const initQuestions = [{
+  type: 'confirm',
+  name: 'isPsuPlugged',
+  message: 'is your arduino connected to the 9v PSU and the usb connected to your computer ?'
+}]
+
+const questions = [
+  {
+    type: 'list',
+    name: 'size',
+    message: 'What size do you need?',
+    choices: ['Large', 'Medium', 'Small'],
+    filter: function (val) {
+      return val.toLowerCase()
+    }
+  }
+]
+
+function initQ () {
+  return inc.prompt(initQuestions).then(function (answers) {
+    console.log(blue(JSON.stringify(answers)))
+    if (answers.isPsuPlugged === true) {
+      createBoard(catBoard)
+    } else {
+      console.log(red('please connect them and retry'))
+      initQ()
+    }
+  }).catch(function (e) {
+    console.log(e) // "zut !"
   })
-})
+}
+
+function createBoard (boardCb, opts) {
+  if (!opts) opts = {repl: false}
+  const board = new five.Board(opts)
+  board.on('ready', boardCb)
+  return board
+}
+
+function calibrate () {
+  console.log(blue('Starting Catbot calibration process'))
+  initQ()
+}
+
+calibrate()
